f859a10a70b5f45ce0d74dd497b69d3b497e21b0
Artem Alekseenko <ivanovo.cwi@gmail.com>
2019-05-15 22:36:59 +0300

    README was upated. JS was commented out.

--- source/js/main.js
+++ source/js/main.js
@@ -1,34 +1,34 @@
-const deadline = 'May 25 2019 00:59:59 GMT+03:00';
+// const deadline = 'May 25 2019 00:59:59 GMT+03:00';
 
-function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor((t / 1000) % 60);
-    const minutes = Math.floor((t / 1000 / 60) % 60);
-    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    return {
-        'total': t,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
+// function getTimeRemaining(endtime) {
+//     const t = Date.parse(endtime) - Date.parse(new Date());
+//     const seconds = Math.floor((t / 1000) % 60);
+//     const minutes = Math.floor((t / 1000 / 60) % 60);
+//     const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//     return {
+//         'total': t,
+//         'hours': hours,
+//         'minutes': minutes,
+//         'seconds': seconds
+//     };
+// }
 
-function initializeTimer(endtime) {
-    const hoursId = document.getElementById('hours');
-    const minId = document.getElementById('min');
-    const secId = document.getElementById('sec');
+// function initializeTimer(endtime) {
+//     const hoursId = document.getElementById('hours');
+//     const minId = document.getElementById('min');
+//     const secId = document.getElementById('sec');
 
-    function updateClock() {
-        const t = getTimeRemaining(endtime);
-        hoursId.innerHTML = ('0' + t.hours).slice(-2);
-        minId.innerHTML = ('0' + t.minutes).slice(-2);
-        secId.innerHTML = ('0' + t.seconds).slice(-2);
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
-        }
-    }
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
-}
+//     function updateClock() {
+//         const t = getTimeRemaining(endtime);
+//         hoursId.innerHTML = ('0' + t.hours).slice(-2);
+//         minId.innerHTML = ('0' + t.minutes).slice(-2);
+//         secId.innerHTML = ('0' + t.seconds).slice(-2);
+//         if (t.total <= 0) {
+//             clearInterval(timeinterval);
+//         }
+//     }
+//     updateClock();
+//     const timeinterval = setInterval(updateClock, 1000);
+// }
 
-initializeTimer(deadline);
+// initializeTimer(deadline);
